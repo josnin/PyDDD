2078c60fc345b8474cddf134cbda4e5c73de158e
josnin <nino.capati@gmail.com>
2024-12-09 21:11:30 +0800

    cleanup

--- app/handlers.py
+++ app/handlers.py
@@ -1,20 +1,23 @@
-from domain.commands import CreateProductCommand
+from domain import commands
 from app import unit_of_work
-from domain.events import ProductCreated
+from domain import events
 from uuid import uuid4
 
-def handle_create_product(command: CreateProductCommand, uow: unit_of_work.UnitOfWork):
+def change_status_command(command: commands.ChangeStatusCommand, uow: unit_of_work.UnitOfWork):
+    print('handle change status')
+
+def handle_create_product(command: commands.CreateProductCommand, uow: unit_of_work.UnitOfWork):
     # handle the command to create a product
     product_id = str(uuid4())
     print("Create product handler here")
-    event = ProductCreated(product_id, command.name, command.price, command.category)
+    event = events.ProductCreated(product_id, command.name, command.price, command.category)
 
     return event
 
-def log_product_created(event: ProductCreated, uow: unit_of_work.UnitOfWork):
+def log_product_created(event: events.ProductCreated, uow: unit_of_work.UnitOfWork):
     # handle the event by logging the product created
     print(f"Product Created: {event.product_id} - {event.name} ({event.price} {event.category})")
 
-def log_product_created2(event: ProductCreated, uow: unit_of_work.UnitOfWork):
+def log_product_created2(event: events.ProductCreated, uow: unit_of_work.UnitOfWork):
     # handle the event by logging the product created
     print(f"Product Created2: {event.product_id} - {event.name} ({event.price} {event.category})")
--- domain/commands.py
+++ domain/commands.py
@@ -1,11 +1,18 @@
 from abc import ABC
 from dataclasses import dataclass
+from domain import enums
+import uuid
 
-class Command(ABC):
-    pass
 
 @dataclass
-class CreateProductCommand(Command):
+class CreateProductCommand:
     name: str
     price: float
     category: str
+
+@dataclass
+class ChangeStatusCommand:
+    product_id: uuid.uuid4
+    new_status: enums.ProductStatus
+
+
