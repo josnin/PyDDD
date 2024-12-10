7997b061de0e5a32f4bbfa3a34ae5fd92ab2b839
josnin <nino.capati@gmail.com>
2024-12-10 21:26:19 +0800

    update handlers unitofwork

--- app/handlers.py
+++ app/handlers.py
@@ -3,10 +3,10 @@ from app import unit_of_work
 from domain import events
 from uuid import uuid4
 
-def change_status_command(command: commands.ChangeStatusCommand, uow: unit_of_work.UnitOfWork):
+def change_status_command(command: commands.ChangeStatusCommand, uow: unit_of_work.DjangoUnitOfWork):
     print('handle change status')
 
-def handle_create_product(command: commands.CreateProductCommand, uow: unit_of_work.UnitOfWork):
+def handle_create_product(command: commands.CreateProductCommand, uow: unit_of_work.DjangoUnitOfWork):
     # handle the command to create a product
     product_id = str(uuid4())
     print("Create product handler here")
@@ -14,10 +14,10 @@ def handle_create_product(command: commands.CreateProductCommand, uow: unit_of_w
 
     return event
 
-def log_product_created(event: events.ProductCreated, uow: unit_of_work.UnitOfWork):
+def log_product_created(event: events.ProductCreated, uow: unit_of_work.DjangoUnitOfWork):
     # handle the event by logging the product created
     print(f"Product Created: {event.product_id} - {event.name} ({event.price} {event.category})")
 
-def log_product_created2(event: events.ProductCreated, uow: unit_of_work.UnitOfWork):
+def log_product_created2(event: events.ProductCreated, uow: unit_of_work.DjangoUnitOfWork):
     # handle the event by logging the product created
     print(f"Product Created2: {event.product_id} - {event.name} ({event.price} {event.category})")
